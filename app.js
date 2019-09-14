1e7a9256afcf49a48e6dae016ad56fc228a72db7
J.Farrow <jack.dempsey.farrow@gmail.com>
2019-09-14 16:13:46 -0400

    Adds cors module for temp enabling of CORS requests

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const http = require('http');
 const express = require('express');
 let app = express();
 const PORT = process.env.NODE_ENV === 'prod' ? 3000 : 8080;
+const path = require('path');
+const cors = require('cors');
+const authRouter = require(path.resolve(__dirname, 'routes', 'login', 'login.js'));
 
+app.use(cors());
+app.use('*', authRouter);
 
 http.createServer(app).listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}; env=${process.env.NODE_ENV}`);

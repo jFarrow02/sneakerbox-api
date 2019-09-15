2a22bc79c1accd9a886732ee704cd4b83e1572db
J.Farrow <jack.dempsey.farrow@gmail.com>
2019-09-14 21:14:25 -0400

    Updates app to listen on port 8081-->ELASTIC BEANSTALK

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const express = require('express');
 let app = express();
-const PORT = process.env.NODE_ENV === 'prod' ? 3000 : 8080;
+const PORT = 8081;
 const path = require('path');
 const cors = require('cors');
 const authRouter = require(path.resolve(__dirname, 'routes', 'login', 'login.js'));
 
 app.use(cors());
+
 app.use('*', authRouter);
 
 http.createServer(app).listen(PORT, ()=>{
